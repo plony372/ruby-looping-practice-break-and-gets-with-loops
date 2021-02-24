c3588bb5214b05fc6b61e595092a80e2edb4771c
MenachemFuterfas <menachemfuterfas@gmail.com>
2021-02-24 16:40:13 -0500

    commit tree 9dbbf7295e63b5e6709ada30788613269b94bfcf

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,12 @@
 
-def levitation_quiz
-	#your code here
+def levitation_quiz(answer)
+	loop do
+		puts "What is the spell that enacts levitation?"
+		if answer ==  "Wingardium Leviosa"
+			puts "You passed the quiz!"
+			
+		end
+	end
 end
 
 
